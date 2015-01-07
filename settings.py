a4f06bae286956d08e55ee42ecca41e654694a00
Jean-Marie Comets <jean.marie.comets@gmail.com>
2015-01-07 10:12:00 +0100

    Increase pagination size

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
             ],
-        'PAGINATE_BY': 100,
+        'PAGINATE_BY': 500,
         }
 
 # CORS
